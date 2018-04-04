ebce9729403a18250a89fd53ed28ae5702135d82
Kirill Shatskiy <shaackiy@gmail.com>
2018-04-04 08:59:23 +0300

    Add test cases.

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,13 @@ function isPlainObject(obj: Object): boolean {
         prototype === Object.prototype;
 }
 
+// Ограничился 4мя объектами т.к. в реальной жизни этого будет более чем достаточно. В крайнем случае можно сделать 2 вызова последовательно.
+export function extend<T extends object>(deep: boolean, target: T): T;
 export function extend<T extends object, S extends object>(deep: boolean, target: T, source: S): T & S;
 export function extend<T extends object, S1 extends object, S2 extends object>(deep: boolean, target: T, source1: S1, source2: S2): T & S1 & S2;
 export function extend<T extends object, S1 extends object, S2 extends object, S3 extends object>(deep: boolean, target: T, source1: S1, source2: S2, source3: S3): T & S1 & S2 & S3;
 export function extend<T extends object, S1 extends object, S2 extends object, S3 extends object, S4 extends object>(deep: boolean, target: T, source1: S1, source2: S2, source3: S3, source4: S4): T & S1 & S2 & S3 & S4;
+export function extend<T extends object>(target: T): T;
 export function extend<T extends object, S extends object>(target: T, source: S): T & S;
 export function extend<T extends object, S1 extends object, S2 extends object>(target: T, source1: S1, source2: S2): T & S1 & S2;
 export function extend<T extends object, S1 extends object, S2 extends object, S3 extends object>(target: T, source1: S1, source2: S2, source3: S3): T & S1 & S2 & S3;
@@ -68,3 +71,34 @@ export function extend(...args: any[]): any {
 
     return target;
 };
+
+// Реальные кейсы
+const t1: {x: number} = extend({x: 1});
+const t2: {x: number, y: string} = extend({x: 1}, {y: '2'});
+const t3: {x: string} = extend({x: 1}, {x: '2'});
+const t4: {x: {y: null, z: undefined}} = extend(true, {}, {x: {y: null, z: undefined}});
+const t5: {x: number} = extend(true, {x: 1});
+const t6: {x: number, y: string} = extend(true, {x: 1}, {y: '2'});
+const t7: {x: string} = extend(true, {x: 1}, {x: '2'});
+const t8: {x: {y: null, z: undefined}} = extend(true, {}, {x: {y: null, z: undefined}});
+
+// Непокрытые вещи вытекающие из свойства JS: все что не является примитивом, является объектом
+// В идеальном мире тут должны быть ошибки
+const p1 = extend(() => 1, () => 's');
+const p2: (string|number)[] = extend([1], ['s']);
+const p3 = extend(WeakSet, Map);
+
+// Ошибки
+extend(1, {string: 1});
+extend({}, Symbol('1'));
+extend(null, {string: 1});
+extend(true, undefined);
+extend(true, undefined);
+extend(true, {}, 1);
+extend({}, {}, 1);
+extend({}, null, {});
+extend({}, {s: 1}, undefined);
+
+// Известные ошибки из за кол-ва объектов
+extend({}, {}, {}, {}, {}, {}, {});
+extend({}, {}, {}, {}, {}, {});
